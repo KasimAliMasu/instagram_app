fb3fdc575bfa20fa4c41fb1ce9dcbec424a6ed94
KasimAliMasu <kasimalimasu@gmail.com>
2024-11-18 17:16:08 +0530

    add new file and folder or modified the old file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_app/responsive/mobile_screen_layout.dart';
-import 'package:instagram_app/responsive/web_screen_layout.dart';
+
+import 'package:instagram_app/screens/login_screen.dart';
+import 'package:instagram_app/screens/signup_screen.dart';
 import 'package:instagram_app/utils/colors.dart';
-import 'responsive/responsive_layout.dart';
 
 void main() async {
-  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBOKGqy39pF6kMcj3LTRYymRKVCZFyscLw",
+        appId: "1:1007118840094:web:7a55194e98d2e41026590f",
+        messagingSenderId: "1007118840094",
+        projectId: "instagramapp-dbe8a",
+        storageBucket: "instagramapp-dbe8a.firebasestorage.app",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -16,15 +31,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'instagram_app',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor,
-      ),
-      home: const ResponsiveLayout(
-        mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout(),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'instagram_app',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor,
+        ),
+        // home: const ResponsiveLayout(
+        //   mobileScreenLayout: MobileScreenLayout(),
+        //   webScreenLayout: WebScreenLayout(),
+        // ),
+        home: SignupScreen());
   }
 }
